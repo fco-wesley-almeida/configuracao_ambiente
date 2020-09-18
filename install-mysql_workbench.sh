83894970dc35466086ffb77940651fa8d6de359a
Wesley Almeida <fco.wesley.almeida@gmail.com>
2020-09-17 23:39:21 -0300

    Revert "Adicionar primeiros softwares"
    This reverts commit 361de0cabea73958f7e83a0f0a5c7e26e1cd07a8.

--- install-mysql_workbench.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt -y install wget
-wget https://dev.mysql.com/get/mysql-apt-config_0.8.15-1_all.deb
-sudo apt install ./mysql-apt-config_0.8.15-1_all.deb
-sudo apt update -y
-sudo apt install mysql-workbench-community -y
-
